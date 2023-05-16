0364af93cf87e1fa5022d6651b02a7549a339afc
ArtemPluktus <artempluktus@gmail.com>
2023-05-16 19:34:25 +0200

    1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,44 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+function createGalleryCardsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+     </a>
+    </li>
+    `;
+    }).join('');
+};
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+galleryContainer.addEventListener(`click`, (e) => {
+    e.preventDefault();
+});
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true, // Відображати підпис під зображеннями
+    captionsData: "alt", // Використовувати значення атрибуту alt для підписів
+    captionDelay: 250, // Затримка перед відображенням підписів (в мілісекундах)
+    captionsSelector: "self", // Використовувати сами елементи зображень як селектор для підписів
+    elementsSelector: ".my-gallery a", // Використовувати інший селектор для елементів галереї
+    disableScroll: true, // Заборонити прокручування сторінки під час перегляду зображень
+    history: false, // Вимкнути запис історії перегляду зображень в браузері
+    closeText: "X", // Текст кнопки закриття модального вікна
+    showCounter: true, // Відображати лічильник зображень
+    slideInterval: 5000, // Інтервал автоматичної зміни зображень (в мілісекундах)
+    disableRightClick: true, // Заборонити клік правою кнопкою миші
+    widthRatio: 0.8, // Відношення ширини великого зображення до вікна браузера
+    heightRatio: 0.8, // Відношення висоти великого зображення до вікна браузера
+    docClose: true // Закривати модальне вікно при кліку на документ
+});
