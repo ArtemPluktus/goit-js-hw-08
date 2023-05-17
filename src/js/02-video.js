ca63bbd3630beb50e905387627efe16ff2e11a55
ArtemPluktus <artempluktus@gmail.com>
2023-05-17 10:05:37 +0200

    1, 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(time => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+}, 1000)); // Обмежуємо частоту оновлення раз на секунду (1000 мілісекунд)
+
+const saveTime = localStorage.getItem('videoplayer-current-time');
+const timeStop = JSON.parse(saveTime);
+
+player.setCurrentTime(timeStop.seconds || 0);
