ce92ca140215940b276e23b7a47f7b64291624fd
ArtemPluktus <artempluktus@gmail.com>
2023-05-20 19:07:46 +0200

    1, 2, 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(`.feedback-form`)
+
+const STORAGE_KEY = `feedback-form-state`;
+
+const formData = {};
+
+lastInput();
+
+form.addEventListener(`input`, (e) => {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener(`submit`, (e) => {
+    e.preventDefault();
+
+    e.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_KEY);
+});
+
+function lastInput() {
+    const savedInput = localStorage.getItem(STORAGE_KEY);
+
+    if (savedInput) {
+        form.value = savedInput;
+    }
+};
